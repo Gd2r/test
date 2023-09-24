5191db678a8ad9d420e2cfbb08da363df890a0e2
Hisham <Mr9r9or@gmail.com>
2023-09-24 18:41:54 +0300

    V0.3
    Reviewed-by: Hisham <mr9r9or@gmail.com>
    I made a new class

--- src/amer.java
+++ src/amer.java
@@ -1,7 +1,8 @@
 public class amer {
 
     public static void main (String[] args) {
-        System.out.println("hello world");
+        System.out.println("hellodassdasdasdorld");
+
         /* this is main */
     }
 }
