01b64281c95459a9e34fed92b3b02c6018d735cb
73azn <amerbinazim@gmail.com>
2023-09-24 18:36:00 +0300

    v0.4
    added new sout

--- src/amer.java
+++ src/amer.java
@@ -2,6 +2,7 @@ public class amer {
 
     public static void main (String[] args) {
         System.out.println("hello world");
+        System.out.println("hello my name is amer");
         /* this is main */
     }
 }
